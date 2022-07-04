b3b0deaa982566388ec90fb0a42373eeefa27a9f
Ryan Caezar Itang <sitiom@disroot.org>
2022-07-04 19:36:13 +0800

    fix: add setup function

--- README.md
+++ README.md
@@ -2,6 +2,25 @@
 
 Neovim plugin to automatically toggle between hybrid and absolute line numbers. Written in Lua.
 
+## Getting started
+
+### Requirements
+
+- Neovim 0.7 or later
+
+### Installation
+
+Use your favorite package manager. Example config with [packer.nvim](https://github.com/wbthomason/packer.nvim):
+
+```lua
+use {
+   "sitiom/nvim-numbertoggle",
+   config = function()
+      require("numbertoggle").setup()
+   end
+}
+```
+
 ## Acknowledgment
 
 https://github.com/jeffkreeftmeijer/vim-numbertoggle
--- lua/numbertoggle.lua
+++ lua/numbertoggle.lua
@@ -1,21 +1,27 @@
-local augroup = vim.api.nvim_create_augroup "numbertoggle"
+local M = {}
 
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
-   pattern = "*",
-   group = augroup,
-   callback = function()
-      if vim.o.nu and vim.api.nvim_get_mode().mode == "i" then
-         vim.wo.relativenumber = true
-      end
-   end,
-})
+function M.setup()
+   local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
 
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
-   pattern = "*",
-   group = augroup,
-   callback = function()
-      if vim.o.nu then
-         vim.wo.relativenumber = false
-      end
-   end,
-})
+   vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
+      pattern = "*",
+      group = augroup,
+      callback = function()
+         if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+            vim.wo.relativenumber = true
+         end
+      end,
+   })
+
+   vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
+      pattern = "*",
+      group = augroup,
+      callback = function()
+         if vim.o.nu then
+            vim.wo.relativenumber = false
+         end
+      end,
+   })
+end
+
+return M

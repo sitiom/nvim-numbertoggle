3212b0958e26cddd389cc6a77cf0de8b2401e97a
sitiom <sitiom@disroot.org>
2022-07-04 18:24:08 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# nvim-numbertoggle
+
+Neovim plugin to automatically toggle between hybrid and absolute line numbers. Written in Lua.
+
+## Acknowledgement
+
+https://github.com/jeffkreeftmeijer/vim-numbertoggle
---
+++ lua/numbertoggle.lua
@@ -0,0 +1,21 @@
+local augroup = vim.api.nvim_create_augroup "numbertoggle"
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
+   pattern = "*",
+   group = augroup,
+   callback = function()
+      if vim.o.nu and vim.api.nvim_get_mode().mode == "i" then
+         vim.wo.relativenumber = true
+      end
+   end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
+   pattern = "*",
+   group = augroup,
+   callback = function()
+      if vim.o.nu then
+         vim.wo.relativenumber = false
+      end
+   end,
+})
